2dc3af5d17394b2e42dd4a6f11b18060f4050422
enomine <enominecn@gmail.com>
2018-05-13 16:37:23 +0800

    if no action send with no message-attributes

--- sns_send.sh
+++ sns_send.sh
@@ -5,8 +5,12 @@ exec >>/var/log/cron.log
 exec 2>&1
 
 if [ $# -eq 1 ]; then
-  action=$(echo "$1" | python -c 'import sys, json; sys.stdout.write(json.load(sys.stdin)["action"].lower())')
-  aws sns publish --topic-arn $AWS_ARN_BUS --message "$1" --message-attributes "{\"action\":{\"DataType\":\"String\",\"StringValue\":\"$action\"}}"
+  action=$(echo "$1" | python -c 'import sys, json; sys.stdout.write(json.load(sys.stdin).get("action", "").lower())')
+  if [ $action ]; then
+    aws sns publish --topic-arn $AWS_ARN_BUS --message "$1" --message-attributes "{\"action\":{\"DataType\":\"String\",\"StringValue\":\"$action\"}}"
+  else
+    aws sns publish --topic-arn $AWS_ARN_BUS --message "$1"
+  fi
 else
   aws sns publish --topic-arn $AWS_ARN_BUS "$@"
 fi
